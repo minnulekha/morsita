315bcbe9def34fadfe9e07cb01c3ec00ef1271f1
minnulekha <minnulekhavg@gmail.com>
2025-07-13 17:54:12 +0530

    Configure static files and whitenoise for Render

--- project/settings.py
+++ project/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -117,12 +118,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Path to your custom static files (already correct)
 STATICFILES_DIRS = [
     BASE_DIR / "app" / "static",
 ]
+
+# This is where Django will collect all static files
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+# Use WhiteNoise to serve compressed static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
