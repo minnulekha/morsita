ce8ee95b6164a824c22b2e8e6b8e01824c0f3dab
minnulekha <minnulekhavg@gmail.com>
2025-07-13 18:21:31 +0530

    Switched to PostgreSQL DB

--- project/settings.py
+++ project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'morsitadb',
+        'USER': 'morsitadb_user',
+        'PASSWORD': 'morsita@morsita',
+        'HOST': 'dpg-d1pqllc9c44c738tgos0-a',
+        'PORT': '5432',
     }
 }
 
